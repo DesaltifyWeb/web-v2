cf565b5efbfe6546e5bd3efadea206c0347f3211
DesaltifyWeb <desaltify.minnesota@gmail.com>
2023-08-03 13:33:08 -0500

    website edit try again for fixing the problem

--- src/components/Redirects/PageNotFound.jsx
+++ src/components/Redirects/PageNotFound.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 
 const PageNotFound = () => {
   useEffect(() => {
-    window.location.href = '/'; // Redirects to the home page
+    window.location.href = ''; // Redirects to the home page
   }, []);
 
   return null;
