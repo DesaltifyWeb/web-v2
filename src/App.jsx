e21319f7903bd894a8f493188e4d8425252a4240
DesaltifyWeb <desaltify.minnesota@gmail.com>
2023-08-05 13:38:06 -0500

    website edit metatags, and plain seo ops

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Routes, Route } from "react-router-dom"
 import Body from './components/Body'
 import Navbar from './components/Navbar'
-import Footer from './components/Footer'
 import MeetTheTeam from './components/MeetTheTeam/MeetTheTeam'
 import ContactUs from './components/ContactUs/ContactUs'
 import Donate from './components/Redirects/Donate'
@@ -14,9 +13,9 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<><Navbar /><Body /><Footer /></>}/>
-        <Route path='/meet' element={<><Navbar /><MeetTheTeam /><Footer /></>}/>
-        <Route path='/contact' element={<><Navbar /><ContactUs /><Footer /></>}/>
+        <Route path='/' element={<><Navbar /><Body /></>}/>
+        <Route path='/meet' element={<><Navbar /><MeetTheTeam /></>}/>
+        <Route path='/contact' element={<><Navbar /><ContactUs /></>}/>
         <Route path='/donate' element={<Donate />}/>
       </Routes>
     </div>
