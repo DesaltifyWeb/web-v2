4eed1bd284c04db522655a96e8d91aa99fdbf992
DesaltifyWeb <desaltify.minnesota@gmail.com>
2023-08-05 13:40:23 -0500

    website edit metatags, and plain seo ops

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { HashRouter } from 'react-router-dom'
-import Navbar from './components/Navbar.jsx'
-import PageNotFound from './components/Redirects/PageNotFound.jsx'
-import Footer from './components/Footer.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
